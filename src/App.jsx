8281295d862187a2b8e64c5a9ab2fdbf2eaafbf5
Santosh Saroj <santoshsaroj0032@gmail.com>
2024-12-23 00:40:14 +0530

    routes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
- import React from 'react'
- 
+ import{BrowserRouter , Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+ import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
  export default function App() {
-   return  <h1 className='text-red-900'>App</h1>;
-     
-   
+   return  <BrowserRouter>
+
+<Routes>
+  <Route path='/' element={<Home/>}></Route>
+  <Route path='/about' element={<About/>}></Route>
+   <Route path='/sign-in' element={<SignIn/>}></Route>
+  <Route path='/profile' element={<Profile/>}></Route>
+  <Route path='/sign-up' element={<SignUp/>}></Route>
+
+
+
+</Routes>
+   </BrowserRouter>;
+    
  }
  
